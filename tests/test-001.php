a0eb219f4827114c912092cb6767b0f872c9222a
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-11-14 08:16:01 -0800

    Update test-001.php

--- tests/test-001.php
+++ tests/test-001.php
@@ -10,13 +10,10 @@ $iv = base64_decode('Eow6GVp/66zvfGsrmQu2ZOp6W2I8Bl7FqLGBSzJfXrM=');
 $start = microtime(true);
 
 for($i = 0;$i < 100;$i++){
-	$padding = strlen($text) % 16 ? 0x10 - strlen($text) % 0x10 : 0;
-	$text = str_pad($text,strlen($text) + $padding,chr(0),STR_PAD_RIGHT);
-
 	$encryption = tg_encrypt_ige($text,$key,$iv);
 	$decryption = tg_decrypt_ige($encryption,$key,$iv);
 
-	assert($encryption === hex2bin('5c11254ddb01d59f37b71c2c12125f888d9014548582692dc4fc163a76d852d5c52733374a27ce2d3f0edef4f0722b7a32183a61b0ab4560dd91f6019ff7fa6c5c66aefe8f173053081a7b01cb50b729c32f56d7c1b4b3ccf87ebd0d96c8e076613f9fa76105ecfe2589a97fbf0f180a36908e54e680cdb8037c0fd38858b9da'));
+	assert(strlen($encryption) == 1024 * 1024);
 	assert($text === $decryption);
 }
 
@@ -25,3 +22,4 @@ $finish = microtime(true);
 echo 'It took ' , $finish - $start , ' seconds' , PHP_EOL;
 
 ?>
+
