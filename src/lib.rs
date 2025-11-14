eb54e4bc5db81beb1ca9c0aa83f344e968fe1d77
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-11-14 08:22:10 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
 
+mod factorizator;
+mod aesige;
+
 use ext_php_rs::prelude::*;
 use ext_php_rs::binary::Binary;
-use std::collections::HashMap;
 
 #[php_const]
-pub const TGCRYPTO_VERSION: &str = "0.0.3";
+pub const TGCRYPTO_VERSION: &str = "0.0.1";
 
 #[php_function]
-pub fn tg_factorize(pq: u64) -> HashMap<&'static str, u64> {
-    let (p,q) = grammers_crypto::factorize::factorize(pq);
-    let mut result = HashMap::new();
-    result.insert("p",p);
-    result.insert("q",q);
-    result
+pub fn tg_factorize(pq: u64) -> Vec<u64> {
+    factorizator::factor(pq)
 }
 
 #[php_function]
@@ -29,9 +27,8 @@ pub fn tg_encrypt_ige(plain: Binary<u8>, key: Binary<u8>, iv: Binary<u8>) -> Res
     let mut iv_array = [0u8; 32];
     iv_array.copy_from_slice(iv_bytes);
 
-    let cipher = grammers_crypto::encrypt_ige(plain_bytes, &key_array, &iv_array);
+    let cipher = aesige::aes_ige_encrypt(plain_bytes, &key_array, &iv_array);
 
-    // Ok(grammers_crypto::hex::to_hex(&cipher))
     Ok(Binary::from(cipher))
 }
 
@@ -48,55 +45,10 @@ pub fn tg_decrypt_ige(cipher: Binary<u8>, key: Binary<u8>, iv: Binary<u8>) -> Re
     let mut iv_array = [0u8; 32];
     iv_array.copy_from_slice(iv_bytes);
 
-    let plain = grammers_crypto::decrypt_ige(cipher_bytes, &key_array, &iv_array);
+    let plain = aesige::aes_ige_decrypt(cipher_bytes, &key_array, &iv_array);
 
-    // Ok(grammers_crypto::hex::to_hex(&plain))
     Ok(Binary::from(plain))
 }
-
-/*
-#[php_class(name = "ObfuscatedCipher")]
-pub struct ObfuscatedCipher {
-    rx: ctr::Ctr128BE<aes::Aes256>,
-    tx: ctr::Ctr128BE<aes::Aes256>,
-}
-
-#[php_impl]
-impl ObfuscatedCipher {
-    #[php_constructor]
-    pub fn new(init: Vec<u8>) -> PhpResult<Self> {
-        let mut buf = [0u8; 64];
-        buf.copy_from_slice(&init);
-        let buf_rev = buf.iter().copied().rev().collect::<Vec<_>>();
-        Ok(Self {
-            rx: ctr::Ctr128BE::<aes::Aes256>::new(
-                GenericArray::from_slice(&buf_rev[8..40]),
-                GenericArray::from_slice(&buf_rev[40..56]),
-            ),
-            tx: ctr::Ctr128BE::<aes::Aes256>::new(
-                GenericArray::from_slice(&buf[8..40]),
-                GenericArray::from_slice(&buf[40..56]),
-            ),
-        })
-    }
-    #[php_method]
-    pub fn encrypt(&mut self, mut data: Vec<u8>) -> Vec<u8> {
-        self.tx.apply_keystream(&mut data);
-        data
-    }
-    #[php_method]
-    pub fn decrypt(&mut self, mut data: Vec<u8>) -> Vec<u8> {
-        self.rx.apply_keystream(&mut data);
-        data
-    }
-}
-*/
-/*
-#[php_module]
-pub fn module(module: ModuleBuilder) -> ModuleBuilder {
-    module
-}
-*/
 
 #[php_module]
 pub fn module(module: ModuleBuilder) -> ModuleBuilder {
@@ -106,6 +58,3 @@ pub fn module(module: ModuleBuilder) -> ModuleBuilder {
         .function(wrap_function!(tg_encrypt_ige))
         .function(wrap_function!(tg_decrypt_ige))
 }
-
-
-
