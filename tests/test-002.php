702626360a4a2e6ad26ecadd695a16d76fedcbfb
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-11-14 08:20:54 -0800

    Update test-002.php

--- tests/test-002.php
+++ tests/test-002.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 $start = microtime(true);
 
 for($i = 0;$i < 100;$i++){
-	list($p,q) = tg_factorize(0x17ED48941A08F981);
+	list($p,$q) = tg_factorize(0x17ED48941A08F981);
 	assert($p === 0x494C553B);
 	assert($q === 0x53911073);
 }
@@ -15,3 +15,4 @@ $finish = microtime(true);
 echo 'It took ' , $finish - $start , ' seconds' , PHP_EOL;
 
 ?>
+
